dda8b8d3ef474d911cc815ac1fd3f9781090e2d4
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-29 17:16:54 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Bbjj
+admin.site.register(Bbjj)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Bbjj(models.Model):
+    'Generated Model'
+    ndkdkd = models.BigIntegerField()
